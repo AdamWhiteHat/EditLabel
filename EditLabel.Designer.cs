cd3eb258460289d1725f57be9a99adc75ac81762
Adam White <arakaska@gmail.com>
2015-01-02 00:58:27 -0700

    EditLabel is editable Label control.
    EditLabel is editable Label control, who's text can be modified at
    run-time by double-clicking on it. Under the hood, it contains a Label
    control and TextBox control. It works like a Label control until you
    double-click it with the mouse. When double-clicked it enters
    'edit-mode' where a TextBox overlays the Label. The control will
    AutoSize to the characters you type in the TextBox, updating the Label's
    size at the same time. You can leave the control's edit mode by hitting
    either enter, the escape key, or letting the control lose focus. After
    editing, the control acts like a label again, with the updated text.

---
+++ EditLabel.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace EditLabelControl
+{
+    partial class EditLabel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlLabel = new System.Windows.Forms.Label();
+            this.ctrlTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ctrlLabel
+            // 
+            this.ctrlLabel.AutoSize = true;
+            this.ctrlLabel.Location = new System.Drawing.Point(3, 3);
+            this.ctrlLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.ctrlLabel.MinimumSize = new System.Drawing.Size(15, 0);
+            this.ctrlLabel.Name = "ctrlLabel";
+            this.ctrlLabel.Size = new System.Drawing.Size(50, 13);
+            this.ctrlLabel.TabIndex = 3;
+            this.ctrlLabel.Text = "editLabel";
+            this.ctrlLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ctrlTextBox
+            // 
+            this.ctrlTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ctrlTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ctrlTextBox.Location = new System.Drawing.Point(3, 3);
+            this.ctrlTextBox.MinimumSize = new System.Drawing.Size(15, 0);
+            this.ctrlTextBox.Name = "ctrlTextBox";
+            this.ctrlTextBox.Size = new System.Drawing.Size(50, 13);
+            this.ctrlTextBox.TabIndex = 2;
+            this.ctrlTextBox.Text = "editLabel";
+            this.ctrlTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ctrlTextBox.Visible = false;
+            this.ctrlTextBox.WordWrap = false;
+            // 
+            // EditLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.ctrlLabel);
+            this.Controls.Add(this.ctrlTextBox);
+            this.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Name = "EditLabel";
+            this.Size = new System.Drawing.Size(56, 19);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ctrlLabel;
+        private System.Windows.Forms.TextBox ctrlTextBox;
+    }
+}
---
+++ EditLabel.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+
+namespace EditLabelControl
+{
+    [Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(IDesigner))]
+    public partial class EditLabel : UserControl
+    {
+        #region Properties
+        // Public Properties
+        public override string Text
+        {
+            get { return ctrlLabel.Text; }
+            set { ChangeText(value); }
+        }
+
+        // Protected Properties
+        protected bool isEditing { get; set; }
+        protected string _save { get; set; }
+
+        #endregion
+
+        #region Constructors and Initializer
+
+        public EditLabel() : this("editLabel1") { }
+        public EditLabel(string Text)
+        {
+            InitializeComponent();
+            this.Text = Text;
+            Initalize_EditAction();
+        }
+
+        void Initalize_EditAction()	// TextBox edit events
+        {
+            // Begin editing
+            ctrlLabel.DoubleClick += EditLabel_OnDoubleClick;
+
+            // End editing events
+            ctrlTextBox.KeyDown += EditLabel_OnKeyPress;
+            ctrlTextBox.LostFocus += EditLabel_EndEditing;
+            Invalidated += EditLabel_EndEditing;
+            Leave += EditLabel_EndEditing;
+
+            ctrlTextBox.Click += delegate(object s, EventArgs e) { ((TextBox)s).Focus(); };
+            ctrlLabel.Click += delegate(object s, EventArgs e) { ((Label)s).Focus(); };
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        void ChangeText(string Text)
+        {
+            ctrlLabel.Text = Text;
+            EditResizeTextbox();
+        }
+
+        void EditResizeTextbox()
+        {
+            ctrlTextBox.Size = ctrlLabel.Size;
+        }
+
+        void EditLabel_OnDoubleClick(object sender, EventArgs e)
+        {
+            EditBegin();
+        }
+
+        void EditToggle()
+        {
+            isEditing = !isEditing; // IsEditing boolean
+            ctrlLabel.Visible = !ctrlLabel.Visible; // Label visibility
+            ctrlTextBox.Visible = !ctrlTextBox.Visible; // TextBox visibility
+        }
+
+        void EditBegin()
+        {
+            if (!isEditing)
+            {
+                EditToggle();
+
+                _save = ctrlLabel.Text;
+                ctrlTextBox.Text = ctrlLabel.Text;
+
+                EditResizeTextbox();
+                ctrlTextBox.Focus();
+            }
+        }
+
+        void EditEnd(bool AcceptChanges = true)
+        {
+            if (isEditing)
+            {
+                EditToggle();
+
+                if (AcceptChanges)
+                    ChangeText(ctrlTextBox.Text);
+                else
+                    ChangeText(_save);
+            }
+        }
+
+        void EditLabel_EndEditing(object sender, EventArgs e)
+        {
+            EditEnd();
+        }
+
+        void EditLabel_OnKeyPress(object sender, KeyEventArgs e)
+        {
+            if (isEditing)
+            {
+                switch (e.KeyData)
+                {
+                    case Keys.Escape:
+                        EditEnd(false);
+                        break;
+
+                    case Keys.Return:
+                        EditEnd();
+                        break;
+
+                    default:
+                        ChangeText(ctrlTextBox.Text);
+                        break;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
